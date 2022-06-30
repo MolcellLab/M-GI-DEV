9eab9d127ab71395f8e372bc8cb2b3bb0fd3cc5c
MolcellLab <105956026+MolcellLab@users.noreply.github.com>
2022-06-30 16:38:57 +0800

    Update process_spaceranger.sh

--- process_spaceranger.sh
+++ process_spaceranger.sh
@@ -1,6 +1,6 @@
-spaceranger_path='./spaceranger-1.2.2'
-transcriptome_path='./refdata-gex-mm10-2020-A'
-fastq_path='./data/filter'
+spaceranger_path='opt/spaceranger-1.2.2'
+transcriptome_path='opt/refdata-gex-mm10-2020-A'
+fastq_path='opt/data/filter'
 
 
 bsub -q TEST-A -n 16 -e run.err -o run.log -x "$spaceranger_path/spaceranger count --id=E135 --transcriptome=$transcriptome_path  --fastqs=$fastq_path --sample=E136 --image=$fastq_path/b1_he.tif --slide=V10T10-322 --area=B1 --localcores=16 --slidefile $spaceranger_path/V10T10-322.gpr --reorient-images"
